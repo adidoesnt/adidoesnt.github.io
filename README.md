3729701540e3c2fd4481a17c79145cc6b678ee4c
Banerjee Aditya <aditya.banerjee@activate.sg>
2023-11-17 13:20:29 +0800

    add fallbacks for ios

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Aditya Banerjee (V3)
+
 This is the third iteration of my personal website.
 It was created using **[Bun](https://bun.sh/)** and **[Astro](https://astro.build/)**.
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,6 @@
-export { DisplayPicture } from "./displayPicture"
-export { Instagram } from "./instagram"
-export { Facebook } from "./facebook"
-export { Gmail as Email } from "./gmail"
-export { Github } from "./github"
-export { Linkedin } from "./linkedin"
+export { DisplayPicture } from './displayPicture';
+export { Instagram } from './instagram';
+export { Facebook } from './facebook';
+export { Gmail as Email } from './gmail';
+export { Github } from './github';
+export { Linkedin } from './linkedin';
