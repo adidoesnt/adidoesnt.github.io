c79ee3f1771427743c1705c37bc51b76bb04bcc9
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-11-21 01:27:09 +0800

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   integrations: [mdx(), tailwind(), react()],
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
