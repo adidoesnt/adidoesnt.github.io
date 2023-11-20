45da69f7ac523aacf29ef5bac799d2def0605c4b
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-11-21 01:46:53 +0800

    add base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import node from "@astrojs/node";
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  integrations: [mdx(), tailwind(), react()],
-  adapter: node({
-    mode: "standalone"
-  })
+    output: 'hybrid',
+    integrations: [mdx(), tailwind(), react()],
+    adapter: node({
+        mode: 'standalone'
+    }),
+    base: '/'
 });
