198c120156d9775b321a2ebaa44b6ef3b9b63026
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-11-21 10:45:29 +0800

    update style for link icon

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -6,7 +6,7 @@ const defaultDescription = 'This project has no description.';
 export const Project = ({ name, description, html_url }: Repository) => {
     return (
         <div className="flex flex-col gap-2 p-4 text-center rounded-sm min-h-[25dvh] overflow-y-auto bg-blue">
-            <Link href={html_url} className="w-8" />
+            <Link href={html_url} className="w-8 h-8" />
             <div>
                 <h2 className="text-md font-bold">{name}</h2>
             </div>
