0fe16d93e0c6d4de8c25e46817c2fe24b771fad7
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-11-21 10:20:32 +0800

    add deployment command for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,17 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'hybrid',
-    integrations: [mdx(), tailwind(), react()],
-    adapter: node({
-        mode: 'standalone'
-    }),
-    base: "/",
-    build: {
-      client: './dist/client',
-      server: "./dist/server"
-    },
-    site: 'https://www.adityabanerjee.me'
+  output: 'hybrid',
+  integrations: [mdx(), tailwind(), react()],
+  adapter: vercel(),
+  base: "/",
+  build: {
+    client: './dist/client',
+    server: "./dist/server"
+  },
+  site: 'https://www.adityabanerjee.me'
 });
